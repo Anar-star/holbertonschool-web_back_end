39f97603ae5d7964f1732b9ccc90a03978507dfe
Anar-star <suvarovanar2@gmail.com>
2025-12-03 10:22:19 +0000

    Commited a Python function that inserts a new document in a collection based on kwargs

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""This module defines the function that adds a new document in the base of kwargs in Python"""
+def insert_school(mongo_collection, **kwargs):
+    """
+    Adds a new document based on kwargs
+    Arguments:
+        mongo_collection: An object for pymongo collection.
+        **kwargs: Any key word arguments that are included like a document.
+    Returns: New _id value of an included document.
+    """
+    return result.inserted_id
