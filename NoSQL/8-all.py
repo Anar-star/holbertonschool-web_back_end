a3693c5487215bb09839ba8adfbb033e85f33a5e
Anar-star <suvarovanar2@gmail.com>
2025-12-03 10:44:49 +0000

    Commited a Python function that lists all documents in a collection

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
-"""Module for listing all documents in a MongoDB collection."""
+"""
+Module to list all documents in a MongoDB collection.
+"""
+
 
 def list_all(mongo_collection):
     """
     Lists all documents in a collection.
-    
+
     Args:
-        mongo_collection: pymongo collection object.
+        mongo_collection: pymongo collection object
 
     Returns:
-        List of documents, or an empty list if none.
+        A list of all documents, or an empty list if none
     """
     return list(mongo_collection.find())
